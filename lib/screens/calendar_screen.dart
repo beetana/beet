975beb4af9019f286cd140673307b63353311fdf
beetana <miracle.stage@gmail.com>
2020-07-27 12:17:01 +0900

    TODO追加

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CalendarScreen extends StatelessWidget {
+  // TODO table_calendarを使ってカレンダー画面のUI作成
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/set_list_screen.dart
+++ lib/screens/set_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SetListScreen extends StatelessWidget {
+  // TODO implicitly_animated_reorderable_listを使ってセットリストのUI作成
   @override
   Widget build(BuildContext context) {
     return Scaffold(

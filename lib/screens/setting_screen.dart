574c8c6e922661944bbad2b205a7f5de1b629abf
beetana <miracle.stage@gmail.com>
2020-07-27 12:17:24 +0900

    設定画面のUI作成

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -7,7 +7,88 @@ class SettingScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('設定'),
       ),
-      body: Container(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(
+              left: 16.0,
+              top: 24.0,
+              bottom: 8.0,
+            ),
+            child: Text(
+              'ユーザー設定',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: ListTile(
+              title: Text('名前の変更'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+          ),
+          Divider(
+            height: 0.5,
+          ),
+          Container(
+            color: Colors.white,
+            child: ListTile(
+              title: Text('アイコンの変更'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: 16.0,
+              top: 24.0,
+              bottom: 8.0,
+            ),
+            child: Text(
+              'アプリについて',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white,
+            child: ListTile(
+              title: Text('プライバシーポリシー'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+          ),
+          Divider(
+            height: 0.5,
+          ),
+          Container(
+            color: Colors.white,
+            child: ListTile(
+              title: Text('利用規約'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+          ),
+          Divider(
+            height: 0.5,
+          ),
+          Container(
+            color: Colors.white,
+            child: ListTile(
+              title: Text('アプリ情報'),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () {},
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

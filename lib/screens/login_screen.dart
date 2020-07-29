41908b77f5942be14d8356c3b63782fa61735972
beetana <miracle.stage@gmail.com>
2020-07-29 17:00:25 +0900

    ログイン画面を新規登録画面を分離

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,119 @@
+import 'package:beet/screens/my_page_screen.dart';
+import 'package:beet/screens/register_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:beet/models/welcome_model.dart';
+
+class LoginScreen extends StatelessWidget {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<WelcomeModel>(
+      create: (_) => WelcomeModel(),
+      child: Scaffold(
+        body: Consumer<WelcomeModel>(builder: (context, model, child) {
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+//              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Container(),
+                ),
+                TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'メールアドレス',
+                  ),
+                  onChanged: (text) {
+                    model.email = text;
+                  },
+                ),
+                TextField(
+                  controller: passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'パスワード',
+                  ),
+                  onChanged: (text) {
+                    model.password = text;
+                  },
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RaisedButton(
+                  elevation: 3.0,
+                  child: Text("ログイン"),
+                  color: Colors.grey,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  onPressed: () async {
+                    try {
+                      await model.login();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => MyPageScreen(),
+                        ),
+                      );
+                    } catch (e) {
+                      _showTextDialog(context, e.toString());
+                    }
+                  },
+                ),
+                Expanded(
+                  flex: 2,
+                  child: (Container()),
+                ),
+                FlatButton(
+                  padding: EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    "アカウントを作成",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => RegisterScreen()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+Future _showTextDialog(context, message) async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        ),
+        title: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}

59aeb5f8ccbe095bd4b7b6d76a6b6f599649bb58
beetana <miracle.stage@gmail.com>
2020-08-06 11:41:40 +0900

    不要なコードを削除

--- lib/models/setting_model.dart
+++ lib/models/setting_model.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:beet/user.dart';
 
 class SettingModel extends ChangeNotifier {
   String userName;
 
   Future init() async {
     final firebaseUser = await FirebaseAuth.instance.currentUser();
-    final DocumentSnapshot userDoc = await Firestore.instance
+    final DocumentSnapshot user = await Firestore.instance
         .collection('users')
         .document(firebaseUser.uid)
         .get();
-    final user = User(userDoc);
-    this.userName = user.name;
+    userName = user['name'];
     notifyListeners();
   }
 }

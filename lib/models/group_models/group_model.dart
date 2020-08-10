65e6f16d6da05adcdb77f125afdc4d4330d5783d
beetana <beetana22beetana@gmail.com>
2020-08-10 23:16:07 +0900

    FloatingActionButtonを追加

--- lib/models/group_models/group_model.dart
+++ lib/models/group_models/group_model.dart
@@ -1,20 +1,39 @@
+import 'package:beet/screens/group_screens/group_calendar_screen.dart';
+import 'package:beet/screens/group_screens/group_main_screen.dart';
+import 'package:beet/screens/group_screens/group_song_list_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class GroupModel extends ChangeNotifier {
   int currentIndex = 0;
-  String groupName;
+  String groupName = '';
+  final List<Widget> switchBody = [
+    GroupMainScreen(),
+    GroupCalendarScreen(),
+    GroupSongListScreen(),
+  ];
 
   Future init({String groupID}) async {
     DocumentSnapshot groupDoc =
         await Firestore.instance.collection('groups').document(groupID).get();
     groupName = groupDoc['groupName'];
-    print(groupDoc);
     notifyListeners();
   }
 
   void onTabTapped(int index) {
     currentIndex = index;
     notifyListeners();
+  }
+
+  Widget switchFAB(int index) {
+    if (index == 0) {
+      return null;
+    } else {
+      return FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blueGrey,
+        onPressed: () {},
+      );
+    }
   }
 }

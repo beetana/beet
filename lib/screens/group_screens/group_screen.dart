f48533e4d3fa7a638ea87a54cd3ee2fe1479306f
beetana <beetana22beetana@gmail.com>
2020-08-10 23:16:07 +0900

    FloatingActionButtonを追加

--- lib/screens/group_screens/group_screen.dart
+++ lib/screens/group_screens/group_screen.dart
@@ -1,69 +1,70 @@
 import 'package:beet/models/group_models/group_model.dart';
 import 'package:beet/screens/drawer_screen.dart';
-import 'package:beet/screens/group_screens/group_calendar_screen.dart';
-import 'package:beet/screens/group_screens/group_main_screen.dart';
 import 'package:beet/screens/setting_screens/group_setting_screen.dart';
-import 'package:beet/screens/group_screens/group_song_list_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class GroupScreen extends StatelessWidget {
   GroupScreen({this.groupID});
   final String groupID;
-  final List<Widget> _children = [
-    GroupMainScreen(),
-    GroupCalendarScreen(),
-    GroupSongListScreen(),
-  ];
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<GroupModel>(
       create: (_) => GroupModel()..init(groupID: groupID),
       child: Consumer<GroupModel>(builder: (context, model, child) {
-        return Scaffold(
-          drawer: DrawerScreen(),
-          appBar: AppBar(
-            title: Text(model.groupName),
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.settings,
+        if (model.groupName.isNotEmpty) {
+          return Scaffold(
+            drawer: DrawerScreen(),
+            appBar: AppBar(
+              title: Text(model.groupName),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.settings,
+                  ),
+                  onPressed: () async {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            GroupSettingScreen(groupID: groupID),
+                        fullscreenDialog: true,
+                      ),
+                    );
+                    model.init(groupID: groupID);
+                  },
                 ),
-                onPressed: () async {
-                  await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          GroupSettingScreen(groupID: groupID),
-                      fullscreenDialog: true,
-                    ),
-                  );
-                  model.init(groupID: groupID);
-                },
-              ),
-            ],
-          ),
-          body: _children[model.currentIndex],
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: model.onTabTapped,
-            currentIndex: model.currentIndex,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('ホーム'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.calendar_today),
-                title: Text('カレンダー'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.library_music),
-                title: Text('持ち曲'),
-              ),
-            ],
-          ),
-        );
+              ],
+            ),
+            body: model.switchBody[model.currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: model.onTabTapped,
+              currentIndex: model.currentIndex,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  title: Text('ホーム'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_today),
+                  title: Text('カレンダー'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.library_music),
+                  title: Text('持ち曲'),
+                ),
+              ],
+            ),
+            floatingActionButton: model.switchFAB(model.currentIndex),
+          );
+        } else {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
       }),
     );
   }

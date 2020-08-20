dc112ee113c7cfe1ef9b61c52117f393a4c4311a
beetana <beetana22beetana@gmail.com>
2020-08-20 14:18:57 +0900

    メイン画面に今日の日付を表示

--- lib/screens/group_screens/group_main_screen.dart
+++ lib/screens/group_screens/group_main_screen.dart
@@ -1,36 +1,48 @@
 import 'package:beet/models/group_models/group_main_model.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class GroupMainScreen extends StatelessWidget {
+  final dateFormat = DateFormat("y.M.d");
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<GroupMainModel>(
       create: (_) => GroupMainModel(),
       child: Consumer<GroupMainModel>(builder: (context, model, child) {
-        return ListView.builder(
-            padding: const EdgeInsets.only(
-              top: 32.0,
-              left: 16.0,
-              right: 16.0,
+        return Column(
+          children: <Widget>[
+            Text(
+              dateFormat.format(DateTime.now()),
+              style: TextStyle(fontSize: 30.0),
             ),
-            itemCount: model.schedules.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: Material(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(5.0),
+            Expanded(
+              child: ListView.builder(
+                  padding: const EdgeInsets.only(
+                    top: 32.0,
+                    left: 16.0,
+                    right: 16.0,
                   ),
-                  textStyle: TextStyle(
-                    fontSize: 30.0,
-                  ),
-                  color: Colors.grey,
-                  child:
-                      Center(child: Text('sample ${model.schedules[index]}')),
-                ),
-              );
-            });
+                  itemCount: model.schedules.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Material(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                        textStyle: TextStyle(
+                          fontSize: 30.0,
+                        ),
+                        color: Colors.grey,
+                        child: Center(
+                            child: Text('sample ${model.schedules[index]}')),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        );
       }),
     );
   }

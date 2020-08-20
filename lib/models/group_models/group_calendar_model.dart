227fba76b13b3bb092d3cd9413a241810119b23e
beetana <beetana22beetana@gmail.com>
2020-08-20 23:04:17 +0900

    DateTime.now()のminuteを切りの良い数字にしたかったので、init関数を作って時間を12時に固定するよう変更

--- lib/models/group_models/group_calendar_model.dart
+++ lib/models/group_models/group_calendar_model.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
 class GroupCalendarModel extends ChangeNotifier {
-  DateTime selectedDay = DateTime.now();
+  DateTime now = DateTime.now();
+  DateTime selectedDay;
+  void init() {
+    selectedDay = DateTime(now.year, now.month, now.day, 12);
+  }
 }

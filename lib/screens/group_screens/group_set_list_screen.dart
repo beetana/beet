67a0b3f83fa6bfb81442b4d34e8ef72296706c40
beetana <beetana22beetana@gmail.com>
2020-09-09 15:19:41 +0900

    セットリスト作成ページの機能追加

--- lib/screens/group_screens/group_set_list_screen.dart
+++ lib/screens/group_screens/group_set_list_screen.dart
@@ -1,111 +1,139 @@
-import 'package:beet/song.dart';
+import 'package:beet/models/group_models/group_set_list_model.dart';
 import 'package:flutter/material.dart';
 import 'package:implicitly_animated_reorderable_list/implicitly_animated_reorderable_list.dart';
+import 'package:provider/provider.dart';
 
 class GroupSetListScreen extends StatelessWidget {
-  GroupSetListScreen({this.setList, this.songNum, this.totalPlayTime});
-  final List<Song> setList;
+  GroupSetListScreen({this.selectedSongs, this.songNum, this.totalPlayTime});
+  final List<String> selectedSongs;
   final int songNum;
   final int totalPlayTime;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('セットリスト作成'),
-      ),
-      body: Stack(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Flexible(
-                child: ImplicitlyAnimatedReorderableList(
-                    items: setList,
-                    areItemsTheSame: (oldItem, newItem) => oldItem == newItem,
-                    onReorderFinished: (song, from, to, songs) {},
-                    itemBuilder: (context, animation, song, index) {
-                      return Reorderable(
-                        key: ValueKey(song),
-                        builder: (context, animation, bool) {
-                          return Material(
-                            type: MaterialType.transparency,
-                            child: ListTile(
-                              title: Text(
-                                '${index + 1}.  ${song.title}',
-                                maxLines: 1,
-                              ),
-                              trailing: Handle(
-                                delay: Duration(milliseconds: 100),
-                                child: Icon(
-                                  Icons.list,
-                                  color: Colors.black54,
-                                ),
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }),
+    return ChangeNotifierProvider<GroupSetListModel>(
+      create: (_) =>
+          GroupSetListModel()..init(selectedSongs, songNum, totalPlayTime),
+      child: Consumer<GroupSetListModel>(builder: (context, model, child) {
+        return Scaffold(
+          appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.white,
               ),
-              SizedBox(
-                height: 35.0,
+              onPressed: () {
+                Navigator.pop(context, model.setList);
+              },
+            ),
+            title: Text('セットリスト作成'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('MCを追加'),
+                onPressed: () {
+                  model.addMC();
+                },
               ),
             ],
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+          body: Stack(
             children: <Widget>[
-              Row(
+              Column(
                 children: <Widget>[
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      height: 40.0,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10.0),
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Text(
-                            '$songNum 曲',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Text(
-                            '$totalPlayTime 分',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ],
-                      ),
-                    ),
+                  Flexible(
+                    child: ImplicitlyAnimatedReorderableList(
+                        items: model.setList,
+                        areItemsTheSame: (oldItem, newItem) =>
+                            oldItem == newItem,
+                        onReorderFinished: (song, from, to, songs) {
+                          model.setList = songs;
+                          print(songs);
+                        },
+                        itemBuilder: (context, animation, song, index) {
+                          return Reorderable(
+                            key: ValueKey(song),
+                            builder: (context, animation, bool) {
+                              return Material(
+                                type: MaterialType.transparency,
+                                child: ListTile(
+                                  title: Text(
+                                    '$song',
+                                    maxLines: 1,
+                                  ),
+                                  trailing: Handle(
+                                    delay: Duration(milliseconds: 100),
+                                    child: Icon(
+                                      Icons.list,
+                                      color: Colors.black54,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }),
                   ),
-                  Expanded(
-                      flex: 2,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: Colors.cyan,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(10.0),
+                  SizedBox(
+                    height: 35.0,
+                  ),
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          height: 40.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10.0),
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Text(
+                                '$songNum 曲',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              Text(
+                                '$totalPlayTime 分',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
                         ),
-                        height: 40.0,
-                        child: FlatButton(
-                          child: Text(
-                            '決定',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          onPressed: () {},
-                        ),
-                      )),
+                      ),
+                      Expanded(
+                          flex: 2,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.cyan,
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10.0),
+                              ),
+                            ),
+                            height: 40.0,
+                            child: FlatButton(
+                              child: Text(
+                                '決定',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              onPressed: () {},
+                            ),
+                          )),
+                    ],
+                  ),
                 ],
               ),
             ],
           ),
-        ],
-      ),
+        );
+      }),
     );
   }
 }

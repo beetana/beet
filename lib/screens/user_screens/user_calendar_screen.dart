aa6757e2f1d77344a1b6e5f8f1102f08e423c660
beetana <beetana22beetana@gmail.com>
2020-10-02 16:30:46 +0900

    ユーザー個人のメインページとカレンダーページを作成

--- lib/screens/user_screens/user_calendar_screen.dart
+++ lib/screens/user_screens/user_calendar_screen.dart
@@ -1,8 +1,103 @@
+import 'package:beet/models/user_models/user_calendar_model.dart';
+import 'package:beet/widgets/event_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:beet/widgets/add_floating_action_button.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class UserCalendarScreen extends StatelessWidget {
+  UserCalendarScreen({this.userID});
+  final String userID;
+  final _calendarController = CalendarController();
+  final Map<DateTime, List> _holidays = {
+    DateTime(2020, 1, 1): ['New Year\'s Day'],
+    DateTime(2020, 2, 14): ['Valentine\'s Day'],
+  };
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ChangeNotifierProvider<UserCalendarModel>(
+      create: (_) => UserCalendarModel()..init(),
+      child: Consumer<UserCalendarModel>(builder: (context, model, child) {
+        return Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                TableCalendar(
+                  startDay: DateTime(1980, 1, 1),
+                  endDay: DateTime(2050, 12, 31),
+                  locale: 'ja_JP',
+                  calendarController: _calendarController,
+                  availableCalendarFormats: {CalendarFormat.month: ''},
+                  events: model.events,
+                  holidays: _holidays,
+                  startingDayOfWeek: StartingDayOfWeek.sunday,
+                  daysOfWeekStyle: DaysOfWeekStyle(
+                    weekdayStyle: TextStyle(color: Colors.black),
+                    weekendStyle: TextStyle(color: Colors.black54),
+                  ),
+                  calendarStyle: CalendarStyle(
+                    weekendStyle: TextStyle(color: Colors.black54),
+                    selectedColor: Colors.cyan[400],
+                    todayColor: Colors.cyan[200],
+                    markersColor: Colors.brown[700],
+                    outsideDaysVisible: false,
+                  ),
+                  headerStyle: HeaderStyle(
+                    centerHeaderTitle: true,
+                    formatButtonVisible: false,
+                  ),
+                  onDaySelected: (DateTime day, List events) {
+                    model.selectedDay =
+                        DateTime(day.year, day.month, day.day, 12);
+                    model.getSelectedEvents();
+                  },
+                  onVisibleDaysChanged: (DateTime first, DateTime last,
+                      CalendarFormat format) async {},
+                  onCalendarCreated: (DateTime first, DateTime last,
+                      CalendarFormat format) async {
+//                    model.getSelectedEvents();
+                  },
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+                    child: ListView.builder(
+                        physics: ScrollPhysics(),
+                        itemExtent: 80.0,
+                        itemCount: model.selectedEvents.length,
+                        itemBuilder: (context, index) {
+                          final event = model.selectedEvents[index];
+                          return EventListTile(
+                            eventID: event.eventID,
+                            eventTitle: event.eventTitle,
+                            eventPlace: event.eventPlace,
+                            eventMemo: event.eventMemo,
+                            isAllDay: event.isAllDay,
+                            startingDateTime: event.startingDateTime,
+                            endingDateTime: event.endingDateTime,
+                            onTap: () {},
+                          );
+                        }),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  AddFloatingActionButton(
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      }),
+    );
   }
 }

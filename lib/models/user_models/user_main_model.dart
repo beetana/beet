3e94e542586e3f9e67ab16a432112fde70dbd31e
beetana <beetana22beetana@gmail.com>
2020-10-19 17:06:22 +0900

    イベント取得時、個人のイベントとグループのイベントを一つの処理で取得できるよう改善

--- lib/models/user_models/user_main_model.dart
+++ lib/models/user_models/user_main_model.dart
@@ -5,17 +5,25 @@ import 'package:flutter/material.dart';
 class UserMainModel extends ChangeNotifier {
   DateTime currentDateTime = DateTime.now();
   List<Event> eventList = [];
-  List<String> joiningGroupList = [];
+  List<String> myIDList = [];
   bool isLoading = false;
 
   Future getEventList(userID) async {
-    final currentTimestamp = Timestamp.fromDate(currentDateTime);
     isLoading = true;
+    final currentTimestamp = Timestamp.fromDate(currentDateTime);
+    myIDList = [userID];
     try {
-      QuerySnapshot eventDoc = await Firestore.instance
+      QuerySnapshot joiningGroupDoc = await Firestore.instance
           .collection('users')
           .document(userID)
-          .collection('events')
+          .collection('joiningGroup')
+          .getDocuments();
+      myIDList.addAll(
+          joiningGroupDoc.documents.map((doc) => doc.documentID).toList());
+
+      QuerySnapshot eventDoc = await Firestore.instance
+          .collectionGroup('events')
+          .where('myID', whereIn: myIDList)
           .where('end', isGreaterThan: currentTimestamp)
           .getDocuments();
       eventList = eventDoc.documents
@@ -31,33 +39,25 @@ class UserMainModel extends ChangeNotifier {
               ))
           .toList();
 
-      QuerySnapshot joiningGroupDoc = await Firestore.instance
-          .collection('users')
-          .document(userID)
-          .collection('joiningGroup')
-          .getDocuments();
-      joiningGroupList =
-          joiningGroupDoc.documents.map((e) => e.documentID).toList();
-      print(joiningGroupList);
       //TODO whereIn句で10件までしかクエリできないので、参加できるグループを5つまでにするなどの対策が必要
-      QuerySnapshot groupEventDoc = await Firestore.instance
-          .collectionGroup('events')
-          .where('groupID', whereIn: joiningGroupList)
-          .where('end', isGreaterThan: currentTimestamp)
-          .getDocuments();
-
-      eventList.addAll(groupEventDoc.documents
-          .map((doc) => Event(
-                eventID: doc.documentID,
-                eventTitle: doc['title'],
-                eventPlace: doc['place'],
-                eventMemo: doc['memo'],
-                isAllDay: doc['isAllDay'],
-                startingDateTime: doc['start'].toDate(),
-                endingDateTime: doc['end'].toDate(),
-                dateList: doc['dateList'].map((date) => date.toDate()).toList(),
-              ))
-          .toList());
+//      QuerySnapshot groupEventDoc = await Firestore.instance
+//          .collectionGroup('events')
+//          .where('groupID', whereIn: myIDList)
+//          .where('end', isGreaterThan: currentTimestamp)
+//          .getDocuments();
+//
+//      eventList.addAll(groupEventDoc.documents
+//          .map((doc) => Event(
+//                eventID: doc.documentID,
+//                eventTitle: doc['title'],
+//                eventPlace: doc['place'],
+//                eventMemo: doc['memo'],
+//                isAllDay: doc['isAllDay'],
+//                startingDateTime: doc['start'].toDate(),
+//                endingDateTime: doc['end'].toDate(),
+//                dateList: doc['dateList'].map((date) => date.toDate()).toList(),
+//              ))
+//          .toList());
 
       eventList
           .sort((a, b) => a.startingDateTime.compareTo(b.startingDateTime));

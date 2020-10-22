7ab6a64de76a68f4ab6ee17fc815c600719ce490
beetana <beetana22beetana@gmail.com>
2020-10-22 21:10:41 +0900

    イベント画面にて、内容を更新して戻ってきた際に新しいデータで画面を再描画できるよう改善

--- lib/models/user_models/user_event_model.dart
+++ lib/models/user_models/user_event_model.dart
@@ -1,27 +1,68 @@
 import 'package:beet/event.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class UserEventModel extends ChangeNotifier {
+  Event event;
   String myID;
+  String eventID;
   String eventTitle = '';
   String eventPlace = '';
   String eventMemo = '';
   bool isAllDay = false;
   DateTime startingDateTime;
   DateTime endingDateTime;
+  bool isLoading = false;
   final dateFormat = DateFormat('y/M/d(E)  H:mm', 'ja_JP');
   final allDayDateFormat = DateFormat('y/M/d(E)', 'ja_JP');
 
   void init(Event event) {
+    this.event = event;
     myID = event.myID;
+    eventID = event.eventID;
     eventTitle = event.eventTitle;
     eventPlace = event.eventPlace;
     eventMemo = event.eventMemo;
     isAllDay = event.isAllDay;
     startingDateTime = event.startingDateTime;
     endingDateTime = event.endingDateTime;
+  }
+
+  Future getEvent({userID}) async {
+    isLoading = true;
+    try {
+      DocumentSnapshot eventDoc = await Firestore.instance
+          .collection('users')
+          .document(userID)
+          .collection('events')
+          .document(eventID)
+          .get();
+      event = Event(
+        eventID: eventDoc.documentID,
+        myID: eventDoc['myID'],
+        eventTitle: eventDoc['title'],
+        eventPlace: eventDoc['place'],
+        eventMemo: eventDoc['memo'],
+        isAllDay: eventDoc['isAllDay'],
+        startingDateTime: eventDoc['start'].toDate(),
+        endingDateTime: eventDoc['end'].toDate(),
+        dateList: eventDoc['dateList'].map((date) => date.toDate()).toList(),
+      );
+      myID = event.myID;
+      eventID = event.eventID;
+      eventTitle = event.eventTitle;
+      eventPlace = event.eventPlace;
+      eventMemo = event.eventMemo;
+      isAllDay = event.isAllDay;
+      startingDateTime = event.startingDateTime;
+      endingDateTime = event.endingDateTime;
+    } catch (e) {
+      print(e.toString());
+    }
+    isLoading = false;
+    notifyListeners();
   }
 
   Widget eventDateWidget() {

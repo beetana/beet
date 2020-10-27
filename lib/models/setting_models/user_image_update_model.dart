00b89ce28b9082042a2e99c1af6341f3ed8bdc0b
beetana <beetana22beetana@gmail.com>
2020-10-27 19:45:42 +0900

    端末内の画像を選んでユーザーのプロフィール画像を登録する機能を追加

---
+++ lib/models/setting_models/user_image_update_model.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImageUpdateModel extends ChangeNotifier {
+  String userID;
+  String userImageURL;
+  File imageFile;
+  bool isLoading = false;
+
+  Future init({userID}) async {
+    isLoading = true;
+    this.userID = userID;
+    DocumentSnapshot user =
+        await Firestore.instance.collection('users').document(userID).get();
+    userImageURL = user['imageURL'];
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future pickImageFile() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    imageFile = File(pickedFile.path);
+    notifyListeners();
+  }
+
+  Future updateUserImage() async {
+    if (imageFile == null) {
+      throw ('ファイルが選択されていません');
+    }
+    try {
+      final storage = FirebaseStorage.instance;
+      StorageTaskSnapshot snapshot = await storage
+          .ref()
+          .child("userImage/$userID")
+          .putFile(imageFile)
+          .onComplete;
+      final imageURL = await snapshot.ref.getDownloadURL();
+      await Firestore.instance
+          .collection('users')
+          .document(userID)
+          .updateData({'imageURL': imageURL});
+      userImageURL = imageURL;
+      notifyListeners();
+    } catch (e) {
+      print(e.toString());
+      throw ('エラーが発生しました');
+    }
+  }
+}
---
+++ lib/screens/setting_screens/user_image_update_screen.dart
@@ -0,0 +1,102 @@
+import 'package:beet/models/setting_models/user_image_update_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UserImageUpdateScreen extends StatelessWidget {
+  UserImageUpdateScreen({this.userID});
+  final String userID;
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<UserImageUpdateModel>(
+      create: (_) => UserImageUpdateModel()..init(userID: userID),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('プロフィール画像を変更'),
+        ),
+        body: Consumer<UserImageUpdateModel>(builder: (context, model, child) {
+          return Stack(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: [
+                    Container(
+                      child: Center(
+                        child: SizedBox(
+                          width: 128.0,
+                          height: 128.0,
+                          child: InkWell(
+                            child: CircleAvatar(
+                              backgroundImage: model.imageFile != null
+                                  ? FileImage(model.imageFile)
+                                  : model.userImageURL != null
+                                      ? NetworkImage(model.userImageURL)
+                                      : AssetImage(
+                                          'images/test_user_image.png'),
+                              backgroundColor: Colors.transparent,
+                            ),
+//                            model.imageFile != null
+//                                ? Image.file(model.imageFile)
+//                                : Container(color: Colors.black),
+                            onTap: () async {
+                              await model.pickImageFile();
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                    RaisedButton(onPressed: () async {
+                      if (model.imageFile != null) {
+                        model.startLoading();
+                        try {
+                          await model.updateUserImage();
+                          await _showTextDialog(context, 'プロフィール画像を保存しました');
+                        } catch (e) {
+                          await _showTextDialog(context, e.toString());
+                        }
+                        model.endLoading();
+                      }
+                    })
+                  ],
+                ),
+              ),
+              model.isLoading
+                  ? Container(
+                      color: Colors.black.withOpacity(0.3),
+                      child: Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    )
+                  : SizedBox(),
+            ],
+          );
+        }),
+      ),
+    );
+  }
+}
+
+Future _showTextDialog(context, message) async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        ),
+        title: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}

13d5e1bab747aacec58aa0cd3800739044caefdc
beetana <beetana22beetana@gmail.com>
2020-10-28 14:34:59 +0900

    プロフィール画像を設定する際、画像をトリミングできる機能を追加した

--- lib/models/setting_models/group_image_update_model.dart
+++ lib/models/setting_models/group_image_update_model.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class GroupImageUpdateModel extends ChangeNotifier {
@@ -13,7 +14,6 @@ class GroupImageUpdateModel extends ChangeNotifier {
   void init({groupID, groupImageURL}) {
     this.groupID = groupID;
     this.groupImageURL = groupImageURL;
-    print(this.groupImageURL);
     notifyListeners();
   }
 
@@ -30,7 +30,29 @@ class GroupImageUpdateModel extends ChangeNotifier {
   Future pickImageFile() async {
     final picker = ImagePicker();
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
-    imageFile = File(pickedFile.path);
+    imageFile = await ImageCropper.cropImage(
+      sourcePath: pickedFile.path,
+      maxWidth: 160,
+      maxHeight: 160,
+      aspectRatio: CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+      ],
+      cropStyle: CropStyle.circle,
+      compressFormat: ImageCompressFormat.png,
+      compressQuality: 100,
+      androidUiSettings: AndroidUiSettings(
+        toolbarTitle: 'プロフィール画像',
+        toolbarColor: Colors.blue,
+        toolbarWidgetColor: Colors.white,
+        lockAspectRatio: true,
+      ),
+      iosUiSettings: IOSUiSettings(
+        title: 'プロフィール画像',
+        doneButtonTitle: '完了',
+        cancelButtonTitle: 'キャンセル',
+      ),
+    );
     notifyListeners();
   }
 
--- lib/models/setting_models/user_image_update_model.dart
+++ lib/models/setting_models/user_image_update_model.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImageUpdateModel extends ChangeNotifier {
@@ -29,7 +30,29 @@ class UserImageUpdateModel extends ChangeNotifier {
   Future pickImageFile() async {
     final picker = ImagePicker();
     final pickedFile = await picker.getImage(source: ImageSource.gallery);
-    imageFile = File(pickedFile.path);
+    imageFile = await ImageCropper.cropImage(
+      sourcePath: pickedFile.path,
+      maxWidth: 160,
+      maxHeight: 160,
+      aspectRatio: CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+      ],
+      cropStyle: CropStyle.circle,
+      compressFormat: ImageCompressFormat.png,
+      compressQuality: 100,
+      androidUiSettings: AndroidUiSettings(
+        toolbarTitle: 'プロフィール画像',
+        toolbarColor: Colors.blue,
+        toolbarWidgetColor: Colors.white,
+        lockAspectRatio: true,
+      ),
+      iosUiSettings: IOSUiSettings(
+        title: 'プロフィール画像',
+        doneButtonTitle: '完了',
+        cancelButtonTitle: 'キャンセル',
+      ),
+    );
     notifyListeners();
   }
 

3a52031e958e2d88c1917196ac1347ad7c6b072e
beetana <beetana22beetana@gmail.com>
2020-10-29 11:50:44 +0900

    プロフィール画像を設定しているかどうかでdrawerに表示される画像を切り替えるようにした

--- lib/screens/setting_screens/group_image_update_screen.dart
+++ lib/screens/setting_screens/group_image_update_screen.dart
@@ -58,7 +58,7 @@ class GroupImageUpdateScreen extends StatelessWidget {
                         }
                         model.endLoading();
                       }
-                    })
+                    }),
                   ],
                 ),
               ),

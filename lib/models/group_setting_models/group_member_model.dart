4cda74e7201f2119396350c7a2fedb5c8df66b2d
beetana <beetana22beetana@gmail.com>
2020-12-03 23:42:05 +0900

    #5 メンバー招待のためのURLを作成するコードを書いてみた

--- lib/models/group_setting_models/group_member_model.dart
+++ lib/models/group_setting_models/group_member_model.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 
 class GroupMemberModel extends ChangeNotifier {
@@ -17,5 +18,27 @@ class GroupMemberModel extends ChangeNotifier {
   void endLoading() {
     isLoading = false;
     notifyListeners();
+  }
+
+  Future createDynamicLink() async {
+    final DynamicLinkParameters parameters = DynamicLinkParameters(
+      uriPrefix: 'https://beet.page.link',
+      link: Uri.parse('https://beet.page.link/?id=$groupID'),
+      androidParameters: AndroidParameters(
+        packageName: 'com.beetana.beet',
+        minimumVersion: 1,
+      ),
+      iosParameters: IosParameters(
+        bundleId: 'com.beetana.beet',
+        minimumVersion: '1',
+      ),
+    );
+
+    Uri dynamicUrl = await parameters.buildUrl();
+    print(dynamicUrl);
+
+//    final ShortDynamicLink shortDynamicLink = await parameters.buildShortLink();
+//    final Uri shortUrl = shortDynamicLink.shortUrl;
+//    print(shortUrl);
   }
 }

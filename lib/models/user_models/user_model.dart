e0f49f91fa227fef16597250ee2907d6f70b5f8a
beetana <beetana22beetana@gmail.com>
2020-12-24 19:54:09 +0900

    #5 送られてきたDynamicLinkを処理する機能を実装

--- lib/models/user_models/user_model.dart
+++ lib/models/user_models/user_model.dart
@@ -1,3 +1,4 @@
+import 'package:beet/dynamic_links_services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -6,8 +7,12 @@ class UserModel extends ChangeNotifier {
   final FirebaseAuth auth = FirebaseAuth.instance;
   String userName = '';
   int currentIndex = 0;
+  final dynamicLinks = DynamicLinksServices();
 
-  Future init({String userID}) async {
+  Future init({String userID, BuildContext context}) async {
+//    this.context = context;
+    dynamicLinks.fetchLinkData(context);
+
     DocumentSnapshot userDoc =
         await FirebaseFirestore.instance.collection('users').doc(userID).get();
     userName = userDoc['name'];

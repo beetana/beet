11840579a825dd21773fc7599b958fac0688979a
beetana <beetana22beetana@gmail.com>
2020-12-24 19:43:34 +0900

    #5 リファクタリング

--- lib/models/group_models/group_main_model.dart
+++ lib/models/group_models/group_main_model.dart
@@ -6,6 +6,8 @@ class GroupMainModel extends ChangeNotifier {
   DateTime currentDateTime = DateTime.now();
   List<Event> eventList = [];
   bool isLoading = false;
+  String invitedGroupID;
+  String invitedGroupName;
 
   Future getEventList(groupID) async {
     final currentTimestamp = Timestamp.fromDate(currentDateTime);
@@ -38,4 +40,37 @@ class GroupMainModel extends ChangeNotifier {
     isLoading = false;
     notifyListeners();
   }
+
+//  void fetchLinkData() async {
+//    try {
+//      PendingDynamicLinkData link =
+//          await FirebaseDynamicLinks.instance.getInitialLink();
+//      handleLinkData(link);
+//      print('いえい');
+//      print('complete getInitialLink');
+//
+//      FirebaseDynamicLinks.instance.onLink(
+//          onSuccess: (PendingDynamicLinkData dynamicLink) async {
+//        handleLinkData(dynamicLink);
+//        print('complete onLink');
+//      });
+//    } catch (e) {
+//      print(e.toString());
+//    }
+//  }
+//
+//  void handleLinkData(PendingDynamicLinkData data) {
+//    final Uri deepLink = data?.link;
+//    if (deepLink != null) {
+//      final queryParams = deepLink.queryParameters;
+//      invitedGroupID = queryParams['id'];
+//      invitedGroupName = queryParams['name'];
+//      print('わーお');
+//      print(queryParams);
+//      print('招待されたグループのIDは$invitedGroupID');
+//      print('招待されたグループの名前は$invitedGroupName');
+//      notifyListeners();
+//      main(invitedGroupID: invitedGroupID, invitedGroupName: invitedGroupName);
+//    }
+//  }
 }

2982a04ee4fa7468b6f29381894b73244d7dde3b
beetana <beetana22beetana@gmail.com>
2021-01-04 22:02:15 +0900

    グループのメンバーページにて、メンバーをリストビューで表示するようにした

--- lib/models/group_setting_models/group_member_model.dart
+++ lib/models/group_setting_models/group_member_model.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class GroupMemberModel extends ChangeNotifier {
+  List<String> userIDs = [];
+  List<String> userNames = [];
+  List<String> userImageURLs = [];
   bool isLoading = false;
 
   void startLoading() {
@@ -11,5 +15,25 @@ class GroupMemberModel extends ChangeNotifier {
   void endLoading() {
     isLoading = false;
     notifyListeners();
+  }
+
+  Future init({groupID}) async {
+    startLoading();
+    try {
+      QuerySnapshot groupUsers = await FirebaseFirestore.instance
+          .collection('groups')
+          .doc(groupID)
+          .collection('groupUsers')
+          .get();
+      userIDs = (groupUsers.docs.map((doc) => doc.id).toList());
+      userNames =
+          (groupUsers.docs.map((doc) => doc['name'].toString()).toList());
+      userImageURLs =
+          (groupUsers.docs.map((doc) => doc['imageURL'].toString()).toList());
+    } catch (e) {
+      print(e);
+    } finally {
+      endLoading();
+    }
   }
 }

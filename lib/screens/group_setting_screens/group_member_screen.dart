e43b777931ab4e5d51150037016faa5338f6bb5b
beetana <beetana22beetana@gmail.com>
2021-01-06 00:13:50 +0900

    #5 自分でグループを退会したり、メンバーをグループから削除する機能を実装

--- lib/screens/group_setting_screens/group_member_screen.dart
+++ lib/screens/group_setting_screens/group_member_screen.dart
@@ -1,5 +1,6 @@
 import 'package:beet/dynamic_links_services.dart';
 import 'package:beet/models/group_setting_models/group_member_model.dart';
+import 'package:beet/screens/user_screens/user_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -32,24 +33,42 @@ class GroupMemberScreen extends StatelessWidget {
                           physics: AlwaysScrollableScrollPhysics(),
                           itemCount: model.userNames.length,
                           itemBuilder: (BuildContext context, int index) {
+                            String userID = model.userIDs[index];
+                            String userName = model.userNames[index];
+                            String userImageURL = model.userImageURLs[index];
                             return ListTile(
                               leading: CircleAvatar(
-                                backgroundImage: model
-                                        .userImageURLs[index].isNotEmpty
-                                    ? NetworkImage(model.userImageURLs[index])
+                                backgroundImage: userImageURL.isNotEmpty
+                                    ? NetworkImage(userImageURL)
                                     : AssetImage('images/test_user_image.png'),
                                 backgroundColor: Colors.transparent,
                               ),
-                              title: Text(model.userNames[index]),
-                              onTap: () {
-//                              Navigator.push(
-//                                context,
-//                                MaterialPageRoute(
-//                                  builder: (context) => GroupScreen(
-//                                    groupID: model.groupID[index],
-//                                  ),
-//                                ),
-//                              );
+                              title: Text(userName),
+                              onTap: () async {
+                                bool isMe = userID == model.myID;
+                                bool isDelete = await _showMemberBottomSheet(
+                                  context,
+                                  isMe,
+                                  userName,
+                                );
+                                if (isDelete == true) {
+                                  model.startLoading();
+                                  try {
+                                    await model.deleteMember(userID: userID);
+                                    isMe
+                                        ? Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (BuildContext context) =>
+                                                  UserScreen(userID: userID),
+                                            ),
+                                          )
+                                        : model.init(groupID: groupID);
+                                  } catch (e) {
+                                    _showTextDialog(context, e.toString());
+                                  }
+                                  model.endLoading();
+                                }
                               },
                             );
                           },
@@ -89,6 +108,105 @@ class GroupMemberScreen extends StatelessWidget {
       }),
     );
   }
+}
+
+Future<bool> _showMemberBottomSheet(
+    BuildContext context, bool isMe, String userName) async {
+  String buttonText = isMe ? 'グループを退会' : 'このメンバーを削除';
+  bool isDelete;
+  await showModalBottomSheet(
+    context: context,
+    isScrollControlled: true,
+    builder: (context) {
+      return SingleChildScrollView(
+        child: Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Container(
+            color: Color(0xff757575),
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    userName,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  FlatButton(
+                    child: Text(
+                      buttonText,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    color: Colors.redAccent,
+                    onPressed: () async {
+                      isDelete =
+                          await _showConfirmDialog(context, isMe, userName);
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+    },
+  );
+  return isDelete;
+}
+
+Future<bool> _showConfirmDialog(context, isMe, userName) async {
+  String titleText = isMe ? 'グループを退会しますか?' : '$userNameをグループから削除しますか?';
+  String buttonText = isMe ? '退会' : '削除';
+  bool isDelete;
+  isDelete = await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        ),
+        title: Text(titleText),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'キャンセル',
+              style: TextStyle(color: Colors.black54),
+            ),
+            onPressed: () {
+              Navigator.pop(context, false);
+            },
+          ),
+          FlatButton(
+            child: Text(
+              buttonText,
+              style: TextStyle(color: Colors.redAccent),
+            ),
+            onPressed: () {
+              Navigator.pop(context, true);
+            },
+          ),
+        ],
+      );
+    },
+  );
+  return isDelete;
 }
 
 Future _showTextDialog(context, message) async {

faea99134a3c01ce2876092d18780a29492208b3
beetana <beetana22beetana@gmail.com>
2021-01-25 16:39:30 +0900

    キーボード外タップでキーボードを閉じれるよう変更した

--- lib/screens/group_screens/group_set_list_screen_2.dart
+++ lib/screens/group_screens/group_set_list_screen_2.dart
@@ -23,127 +23,133 @@ class GroupSetListScreen2 extends StatelessWidget {
     return ChangeNotifierProvider<GroupSetListModel2>(
       create: (_) => GroupSetListModel2()..init(),
       child: Consumer<GroupSetListModel2>(builder: (context, model, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('詳細'),
-            centerTitle: true,
-          ),
-          body: Stack(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        controller: eventTitleController,
-                        decoration: InputDecoration(hintText: 'イベントタイトル'),
-                        onTap: () {
-                          if (model.isShowEventDatePicker == true) {
+        return GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text('詳細'),
+              centerTitle: true,
+            ),
+            body: Stack(
+              children: <Widget>[
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          controller: eventTitleController,
+                          decoration: InputDecoration(hintText: 'イベントタイトル'),
+                          onTap: () {
+                            if (model.isShowEventDatePicker == true) {
+                              model.showEventDatePicker();
+                            }
+                          },
+                          onChanged: (text) {
+                            model.eventTitle = text;
+                          },
+                        ),
+                        TextField(
+                          controller: eventPlaceController,
+                          decoration: InputDecoration(hintText: '会場'),
+                          onTap: () {
+                            if (model.isShowEventDatePicker == true) {
+                              model.showEventDatePicker();
+                            }
+                          },
+                          onChanged: (text) {
+                            model.eventPlace = text;
+                          },
+                        ),
+                        ListTile(
+                          title: Text('日付'),
+                          trailing: Text(model.eventDateText),
+                          onTap: () {
+                            FocusScope.of(context).unfocus();
                             model.showEventDatePicker();
-                          }
-                        },
-                        onChanged: (text) {
-                          model.eventTitle = text;
-                        },
-                      ),
-                      TextField(
-                        controller: eventPlaceController,
-                        decoration: InputDecoration(hintText: '会場'),
-                        onTap: () {
-                          if (model.isShowEventDatePicker == true) {
-                            model.showEventDatePicker();
-                          }
-                        },
-                        onChanged: (text) {
-                          model.eventPlace = text;
-                        },
-                      ),
-                      ListTile(
-                        title: Text('日付'),
-                        trailing: Text(model.eventDateText),
-                        onTap: () {
-                          FocusScope.of(context).unfocus();
-                          model.showEventDatePicker();
-                        },
-                      ),
-                      model.eventDatePickerBox,
-                      Divider(
-                        thickness: 0.4,
-                        height: 0.4,
-                        color: Colors.black,
-                      ),
-                    ],
+                          },
+                        ),
+                        model.eventDatePickerBox,
+                        Divider(
+                          thickness: 0.4,
+                          height: 0.4,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Divider(
-                    thickness: 0.1,
-                    height: 0.1,
-                    color: Colors.grey[800],
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                          height: 40.0,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Text('$songNum 曲'),
-                              Text('$totalPlayTime 分'),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 40.0,
-                        child: VerticalDivider(
-                          thickness: 0.2,
-                          width: 0.2,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Container(
-                          height: 40.0,
-                          child: FlatButton(
-                            child: Text(
-                              '決定',
-                              style: TextStyle(
-                                color: Colors.blueAccent,
-                                fontSize: 17.0,
-                              ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Divider(
+                      thickness: 0.1,
+                      height: 0.1,
+                      color: Colors.grey[800],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Expanded(
+                          flex: 3,
+                          child: Container(
+                            height: 40.0,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                Text('$songNum 曲'),
+                                Text('$totalPlayTime 分'),
+                              ],
                             ),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => GroupSetListScreen3(
-                                    setList: setList,
-                                    eventTitle: model.eventTitle,
-                                    eventPlace: model.eventPlace,
-                                    eventDateText: model.eventDateText,
-                                    songNum: songNum,
-                                    totalPlayTime: totalPlayTime,
-                                    groupID: groupID,
-                                  ),
-                                ),
-                              );
-                            },
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                        Container(
+                          height: 40.0,
+                          child: VerticalDivider(
+                            thickness: 0.2,
+                            width: 0.2,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            height: 40.0,
+                            child: FlatButton(
+                              child: Text(
+                                '決定',
+                                style: TextStyle(
+                                  color: Colors.blueAccent,
+                                  fontSize: 17.0,
+                                ),
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => GroupSetListScreen3(
+                                      setList: setList,
+                                      eventTitle: model.eventTitle,
+                                      eventPlace: model.eventPlace,
+                                      eventDateText: model.eventDateText,
+                                      songNum: songNum,
+                                      totalPlayTime: totalPlayTime,
+                                      groupID: groupID,
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         );
       }),

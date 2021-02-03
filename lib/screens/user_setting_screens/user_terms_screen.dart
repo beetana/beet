b15c463a74774e511dce697cc1eb4288711a4dc4
beetana <beetana22beetana@gmail.com>
2021-02-03 16:48:03 +0900

    #3 不要なコードを削除

--- lib/screens/user_setting_screens/user_terms_screen.dart
+++ lib/screens/user_setting_screens/user_terms_screen.dart
@@ -4,7 +4,6 @@ import 'package:webview_flutter/webview_flutter.dart';
 class UserTermsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String text = '';
     return Scaffold(
       appBar: AppBar(
         title: Text('利用規約'),

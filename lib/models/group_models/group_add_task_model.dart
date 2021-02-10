13b5f355c5c2fe99873bd71fac61149f169916e5
beetana <beetana22beetana@gmail.com>
2021-02-10 15:51:15 +0900

    グループのタスクを登録する機能を作成

---
+++ lib/models/group_models/group_add_task_model.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+class GroupAddTaskModel extends ChangeNotifier {
+  String groupID = '';
+  String taskTitle = '';
+  String dueDateText = '';
+  List<String> assignedPersonIDs = [];
+  List<String> userIDs = [];
+  List<String> userNames = [];
+  List<String> userImageURLs = [];
+  bool isLoading = false;
+  bool isShowDueDatePicker = false;
+  Widget dueDatePickerBox = SizedBox();
+  DateTime now = DateTime.now();
+  DateTime dueDate;
+  final DateFormat dateFormat = DateFormat('y/M/d(E)', 'ja_JP');
+
+  startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void init({String groupID}) async {
+    startLoading();
+    this.groupID = groupID;
+    this.dueDate = DateTime(now.year, now.month, now.day, 12);
+    this.dueDateText = dateFormat.format(dueDate);
+    try {
+      QuerySnapshot groupUsers = await FirebaseFirestore.instance
+          .collection('groups')
+          .doc(groupID)
+          .collection('groupUsers')
+          .get();
+      userIDs = (groupUsers.docs.map((doc) => doc.id).toList());
+      userNames =
+          (groupUsers.docs.map((doc) => doc['name'].toString()).toList());
+      userImageURLs =
+          (groupUsers.docs.map((doc) => doc['imageURL'].toString()).toList());
+    } catch (e) {
+      print(e);
+    } finally {
+      endLoading();
+    }
+  }
+
+  void assignPerson(userID) {
+    if (assignedPersonIDs.contains(userID)) {
+      assignedPersonIDs.remove(userID);
+    } else {
+      assignedPersonIDs.add(userID);
+    }
+    print(assignedPersonIDs);
+    notifyListeners();
+  }
+
+  void showDueDatePicker() {
+    if (isShowDueDatePicker == false) {
+      dueDatePickerBox = Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Container(
+            height: 100.0,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              initialDateTime: dueDate,
+              minimumDate: DateTime(1980, 1, 1),
+              maximumDate: DateTime(2050, 12, 31),
+              onDateTimeChanged: (DateTime newDate) {
+                this.dueDate =
+                    DateTime(newDate.year, newDate.month, newDate.day, 12);
+                this.dueDateText = dateFormat.format(dueDate);
+              },
+            ),
+          ),
+          FlatButton.icon(
+            icon: Icon(
+              Icons.clear,
+              color: Colors.black54,
+            ),
+            label: Text(
+              '未定',
+              style: TextStyle(color: Colors.black54),
+            ),
+            onPressed: () {
+              this.dueDate = DateTime(now.year, now.month, now.day, 12);
+              this.dueDateText = '';
+              this.dueDatePickerBox = SizedBox();
+              this.isShowDueDatePicker = !isShowDueDatePicker;
+              notifyListeners();
+            },
+          ),
+        ],
+      );
+    } else {
+      dueDatePickerBox = SizedBox();
+    }
+    isShowDueDatePicker = !isShowDueDatePicker;
+    notifyListeners();
+  }
+
+  Future addTask() async {
+    if (taskTitle.isEmpty) {
+      throw ('やることを入力してください');
+    }
+    try {
+      await FirebaseFirestore.instance
+          .collection('groups')
+          .doc(groupID)
+          .collection('tasks')
+          .add({
+        'title': taskTitle,
+        'dueDate': Timestamp.fromDate(dueDate),
+        'assignedPersonIDs': assignedPersonIDs,
+        'isCompleted': false,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    } catch (e) {
+      print(e);
+      throw ('エラーが発生しました');
+    }
+  }
+}
---
+++ lib/screens/group_screens/group_add_task_screen.dart
@@ -0,0 +1,155 @@
+import 'package:beet/models/group_models/group_add_task_model.dart';
+import 'package:beet/widgets/assign_task_list_tile.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class GroupAddTaskScreen extends StatelessWidget {
+  GroupAddTaskScreen({this.groupID});
+  final String groupID;
+  final taskTitleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<GroupAddTaskModel>(
+      create: (_) => GroupAddTaskModel()..init(groupID: groupID),
+      child: Consumer<GroupAddTaskModel>(builder: (context, model, child) {
+        return Stack(
+          children: [
+            GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+              },
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text('タスクを追加'),
+                  centerTitle: true,
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text(
+                        '追加',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      onPressed: () async {
+                        model.startLoading();
+                        try {
+                          await model.addTask();
+                          Navigator.pop(context);
+                        } catch (e) {
+                          _showTextDialog(context, e.toString());
+                        }
+                        model.endLoading();
+                      },
+                    ),
+                  ],
+                ),
+                body: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
+                        child: TextField(
+                          controller: taskTitleController,
+                          decoration: InputDecoration(hintText: 'やること'),
+                          onTap: () {
+                            if (model.isShowDueDatePicker == true) {
+                              model.showDueDatePicker();
+                            }
+                          },
+                          onChanged: (text) {
+                            model.taskTitle = text;
+                          },
+                        ),
+                      ),
+                      ListTile(
+                        title: Text('いつまでに'),
+                        trailing: Text(model.dueDateText),
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                          model.showDueDatePicker();
+                        },
+                      ),
+                      model.dueDatePickerBox,
+                      Divider(height: 0.5),
+                      SizedBox(height: 16.0),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Text(
+                          'だれが',
+                          style: TextStyle(fontSize: 17.0),
+                        ),
+                      ),
+                      SizedBox(height: 8.0),
+                      Container(
+                        height: 300,
+                        child: ListView.builder(
+                          physics: BouncingScrollPhysics(),
+                          itemCount: model.userNames.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            String userID = model.userIDs[index];
+                            String userName = model.userNames[index];
+                            String userImageURL = model.userImageURLs[index];
+                            return AssignTaskListTile(
+                              userName: userName,
+                              userImageURL: userImageURL,
+                              isChecked:
+                                  model.assignedPersonIDs.contains(userID),
+                              checkboxCallback: (state) {
+                                model.assignPerson(userID);
+                              },
+                              tileTappedCallback: () {
+                                model.assignPerson(userID);
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                      Divider(height: 0.5),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            model.isLoading
+                ? Container(
+                    color: Colors.black.withOpacity(0.3),
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  )
+                : SizedBox(),
+          ],
+        );
+      }),
+    );
+  }
+}
+
+Future _showTextDialog(context, message) async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          ),
+        ),
+        title: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}

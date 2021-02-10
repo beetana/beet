73cec769108b268e9422eeb990a608ef297458fa
beetana <beetana22beetana@gmail.com>
2021-02-10 16:11:46 +0900

    グループのタスクを登録する機能を作成

---
+++ lib/widgets/assign_task_list_tile.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class AssignTaskListTile extends StatelessWidget {
+  final String userName;
+  final String userImageURL;
+  final bool isChecked;
+  final Function checkboxCallback;
+  final Function tileTappedCallback;
+
+  AssignTaskListTile(
+      {this.userName,
+      this.userImageURL,
+      this.isChecked,
+      this.checkboxCallback,
+      this.tileTappedCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: userImageURL.isNotEmpty
+            ? NetworkImage(userImageURL)
+            : AssetImage('images/test_user_image.png'),
+        backgroundColor: Colors.transparent,
+      ),
+      title: Text(
+        userName,
+        overflow: TextOverflow.ellipsis,
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.grey[800],
+        value: isChecked,
+        onChanged: checkboxCallback,
+      ),
+      onTap: tileTappedCallback,
+    );
+  }
+}

80b8e64f3599a00f5c85893ffd8ac195dd882037
beetana <beetana22beetana@gmail.com>
2021-02-16 17:55:32 +0900

    頻出する色をconstantsファイルにまとめた

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+/// primaryColorの候補
+// 0xFF0B5DA3
+// 0xFF2D3146
+// 0xFF415C59
+// 0xFF135589
+// 0xFF46494E
+
+const kPrimaryColor = Color(0xFF424242);
+const kSlightlyTransparentPrimaryColor = Color(0xCC424242);
+const kTransparentPrimaryColor = Color(0x66424242);
+const kBackGroundColor = Color(0xFFF5F5F5);

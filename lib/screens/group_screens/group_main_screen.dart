4e8bb0389792d0ed4e6c0ccd11b08bfaf9b2b91a
beetana <beetana22beetana@gmail.com>
2021-02-19 14:55:04 +0900

    グループのメイン画面とカレンダー画面のUIを整えた

--- lib/screens/group_screens/group_main_screen.dart
+++ lib/screens/group_screens/group_main_screen.dart
@@ -1,6 +1,8 @@
+import 'package:beet/constants.dart';
 import 'package:beet/models/group_models/group_main_model.dart';
 import 'package:beet/screens/group_screens/group_event_screen.dart';
-import 'package:beet/widgets/group_event_list_tile.dart';
+import 'package:beet/widgets/basic_divider.dart';
+import 'package:beet/widgets/event_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -16,49 +18,106 @@ class GroupMainScreen extends StatelessWidget {
       create: (_) => GroupMainModel()..getEventList(groupID),
       child: Consumer<GroupMainModel>(builder: (context, model, child) {
         if (model.eventList.isNotEmpty) {
-          return Column(
-            children: <Widget>[
-              SizedBox(height: 8.0),
-              Text(
-                dateFormat.format(model.currentDateTime),
-                style: TextStyle(fontSize: 24.0),
-              ),
-              SizedBox(height: 8.0),
-              Divider(
-                thickness: 0.1,
-                height: 0.1,
-                color: Colors.grey[800],
-              ),
-              Expanded(
-                child: ListView.builder(
-                  physics: AlwaysScrollableScrollPhysics(),
-                  itemExtent: 96.0,
-                  itemCount: model.eventList.length,
-                  itemBuilder: (context, index) {
-                    final event = model.eventList[index];
-                    return GroupEventListTile(
-                      eventTitle: event.eventTitle,
-                      eventPlace: event.eventPlace,
-                      isAllDay: event.isAllDay,
-                      startingDateTime: event.startingDateTime,
-                      endingDateTime: event.endingDateTime,
-                      onTap: () async {
-                        await Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => GroupEventScreen(
-                              groupID: groupID,
-                              event: event,
-                            ),
+          return Padding(
+            padding: EdgeInsets.only(top: 8.0),
+            child: Column(
+              children: [
+                Container(
+                  height: 112.0,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          dateFormat.format(model.currentDateTime),
+                          style: TextStyle(
+                            fontSize: 28.0,
+                            fontWeight: FontWeight.w500,
+                            color: kPrimaryColor,
                           ),
-                        );
-                        await model.getEventList(groupID);
-                      },
-                    );
-                  },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              'スケジュール',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.w500,
+                                color: kPrimaryColor,
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  '未完了のタスク',
+                                  style: TextStyle(
+                                    color: kDullGreenColor,
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      '3',
+                                      style: TextStyle(
+                                        fontSize: 24.0,
+                                        color: kDullGreenColor,
+                                      ),
+                                    ),
+                                    Text(
+                                      '件',
+                                      style: TextStyle(
+                                        color: kDullGreenColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                BasicDivider(),
+                Expanded(
+                  child: Scrollbar(
+                    child: ListView.builder(
+                      physics: AlwaysScrollableScrollPhysics(),
+                      itemExtent: 64.0,
+                      itemCount: model.eventList.length,
+                      itemBuilder: (context, index) {
+                        final event = model.eventList[index];
+                        return EventListTile(
+                          eventTitle: event.eventTitle,
+                          eventPlace: event.eventPlace,
+                          isAllDay: event.isAllDay,
+                          startingDateTime: event.startingDateTime,
+                          endingDateTime: event.endingDateTime,
+                          onTap: () async {
+                            await Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => GroupEventScreen(
+                                  groupID: groupID,
+                                  event: event,
+                                ),
+                              ),
+                            );
+                            await model.getEventList(groupID);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           );
         } else if (model.isLoading == true) {
           return Center(

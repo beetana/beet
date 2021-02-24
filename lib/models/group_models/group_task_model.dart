31446075afd2c3a6cc07063a201c5217b85c7ff8
beetana <beetana22beetana@gmail.com>
2021-02-24 23:26:33 +0900

    グループのタスク管理画面を作成し、タスクを追加したり、完了・未完了に振り分ける機能を実装

---
+++ lib/models/group_models/group_task_model.dart
@@ -0,0 +1,107 @@
+import 'package:beet/task.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class GroupTaskModel extends ChangeNotifier {
+  final firestore = FirebaseFirestore.instance;
+  String groupID = '';
+  Map<String, String> memberImages = {};
+  List<Task> tasks = [];
+  List<Task> completedTasks = [];
+  List<Task> notCompletedTasks = [];
+  List<Task> changeStateTasks = [];
+  bool isLoading = false;
+
+  void startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future init({String groupID}) async {
+    startLoading();
+    this.groupID = groupID;
+    try {
+      QuerySnapshot groupUsers = await firestore
+          .collection('groups')
+          .doc(groupID)
+          .collection('groupUsers')
+          .get();
+      groupUsers.docs.map((doc) => memberImages[doc.id] = doc['imageURL']);
+      final userIDs = (groupUsers.docs.map((doc) => doc.id).toList());
+      final userImageURLs =
+          (groupUsers.docs.map((doc) => doc['imageURL']).toList());
+      for (int i = 0; i < userIDs.length; i++) {
+        memberImages[userIDs[i]] = userImageURLs[i];
+      }
+      getTaskList(groupID: groupID);
+    } catch (e) {
+      print(e);
+    }
+    endLoading();
+  }
+
+  Future getTaskList({String groupID}) async {
+    completedTasks = [];
+    notCompletedTasks = [];
+    changeStateTasks = [];
+    try {
+      QuerySnapshot taskDoc = await firestore
+          .collection('groups')
+          .doc(groupID)
+          .collection('tasks')
+          .get();
+      tasks = taskDoc.docs
+          .map((doc) => Task(
+                id: doc.id,
+                title: doc['title'],
+                dueDate: doc['dueDate'].toDate(),
+                assignedMembers: doc['assignedMembers'],
+                isCompleted: doc['isCompleted'],
+              ))
+          .toList();
+      tasks.forEach((task) {
+        task.isCompleted
+            ? completedTasks.add(task)
+            : notCompletedTasks.add(task);
+      });
+      completedTasks.sort((a, b) => a.dueDate.compareTo(b.dueDate));
+      notCompletedTasks.sort((a, b) => a.dueDate.compareTo(b.dueDate));
+    } catch (e) {
+      print(e);
+    }
+    notifyListeners();
+  }
+
+  Future updateCheckState() async {
+    try {
+      final batch = firestore.batch();
+      changeStateTasks.forEach((task) {
+        final taskDocRef = firestore
+            .collection('groups')
+            .doc(groupID)
+            .collection('tasks')
+            .doc(task.id);
+        batch.update(taskDocRef, {
+          'isCompleted': task.isCompleted,
+        });
+      });
+      await batch.commit();
+    } catch (e) {
+      print(e);
+      throw ('エラーが発生しました');
+    }
+  }
+
+  void toggleCheckState(Task task) {
+    task.toggleCheckState();
+    changeStateTasks.contains(task)
+        ? changeStateTasks.remove(task)
+        : changeStateTasks.add(task);
+    notifyListeners();
+  }
+}

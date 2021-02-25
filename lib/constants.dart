36956af3127308552a273473f32e18da36e342f1
beetana <beetana22beetana@gmail.com>
2021-02-25 23:14:41 +0900

    削除ボタンの色を追加

--- lib/constants.dart
+++ lib/constants.dart
@@ -16,3 +16,4 @@ const kDullWhiteColor = Color(0xFFF5F5F5);
 const kTransparentDullWhiteColor = Color(0x55E5E5E5);
 const kEnterButtonColor = Color(0xFF448AFF);
 const kInvalidEnterButtonColor = Color(0x55448AFF);
+const kDeleteButtonColor = Color(0xFFFF5252);

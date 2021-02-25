c527ea06c12a9d5bbc50a576e7f91c466aeea882
beetana <beetana22beetana@gmail.com>
2021-02-25 15:21:06 +0900

    スワイプ等で前の画面に戻したくない画面をWillPopScopeでラップして戻らないようにした

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:beet/constants.dart';
 import 'package:beet/dynamic_links_services.dart';
 import 'package:beet/screens/login_screen.dart';
 import 'package:beet/screens/register_screen.dart';
+import 'package:beet/will_pop_callback.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
@@ -19,88 +20,91 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            TypewriterAnimatedTextKit(
-              text: ['beet'],
-              textStyle: TextStyle(
-                fontSize: 72.0,
-                fontWeight: FontWeight.w900,
-                color: kPrimaryColor,
+    return WillPopScope(
+      onWillPop: willPopCallback,
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TypewriterAnimatedTextKit(
+                text: ['beet'],
+                textStyle: TextStyle(
+                  fontSize: 72.0,
+                  fontWeight: FontWeight.w900,
+                  color: kPrimaryColor,
+                ),
+                speed: Duration(milliseconds: 480),
+                isRepeatingAnimation: false,
               ),
-              speed: Duration(milliseconds: 480),
-              isRepeatingAnimation: false,
-            ),
-            SizedBox(height: 120),
-            Container(
-              height: 56.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: FlatButton(
-                shape: RoundedRectangleBorder(
+              SizedBox(height: 120),
+              Container(
+                height: 56.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                color: kPrimaryColor,
-                highlightColor: Colors.white38,
-                child: Text(
-                  'ログイン',
-                  style: TextStyle(
-                    color: Color(0xFFf5f5f5),
-                    fontWeight: FontWeight.bold,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LoginScreen(),
-                      fullscreenDialog: true,
+                  color: kPrimaryColor,
+                  highlightColor: Colors.white38,
+                  child: Text(
+                    'ログイン',
+                    style: TextStyle(
+                      color: Color(0xFFf5f5f5),
+                      fontWeight: FontWeight.bold,
                     ),
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 12.0),
-            Container(
-              height: 56.0,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 1.0,
-                  color: Colors.grey[800],
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginScreen(),
+                        fullscreenDialog: true,
+                      ),
+                    );
+                  },
                 ),
-                borderRadius: BorderRadius.circular(10.0),
               ),
-              child: FlatButton(
-                shape: RoundedRectangleBorder(
+              SizedBox(height: 12.0),
+              Container(
+                height: 56.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                    color: Colors.grey[800],
+                  ),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
-                child: Text(
-                  'アカウントを作成',
-                  style: TextStyle(
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.bold,
+                child: FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => RegisterScreen(),
-                      fullscreenDialog: true,
+                  child: Text(
+                    'アカウントを作成',
+                    style: TextStyle(
+                      color: kPrimaryColor,
+                      fontWeight: FontWeight.bold,
                     ),
-                  );
-                },
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => RegisterScreen(),
+                        fullscreenDialog: true,
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

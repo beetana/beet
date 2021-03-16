f62bdddc49cc6572a6d10a2243defdeb1820e0c0
beetana <beetana22beetana@gmail.com>
2021-03-16 20:31:32 +0900

    リファクタリング

--- lib/screens/user_setting_screens/user_setting_screen.dart
+++ lib/screens/user_setting_screens/user_setting_screen.dart
@@ -4,6 +4,7 @@ import 'package:beet/screens/user_setting_screens/user_privacy_policy_screen.dar
 import 'package:beet/screens/user_setting_screens/user_security_screen.dart';
 import 'package:beet/screens/user_setting_screens/user_profile_screen.dart';
 import 'package:beet/screens/user_setting_screens/user_terms_screen.dart';
+import 'package:beet/utilities/show_app_info_dialog.dart';
 import 'package:beet/widgets/thin_divider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -120,7 +121,7 @@ class UserSettingScreen extends StatelessWidget {
                 title: Text('アプリの詳細'),
                 trailing: Icon(Icons.keyboard_arrow_right),
                 onTap: () {
-                  _showAppInfoDialog(context);
+                  showAppInfoDialog(context);
                 },
               ),
               Expanded(
@@ -134,66 +135,4 @@ class UserSettingScreen extends StatelessWidget {
       ),
     );
   }
-}
-
-Future _showAppInfoDialog(context) async {
-  await showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage('images/app_icon.png'),
-            ),
-            SizedBox(width: 16.0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'beet',
-                  style: TextStyle(
-                    fontSize: 26.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-                Text(
-                  '1.0.0',
-                  style: TextStyle(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Text(
-                  '2021  Kohei Tanabe',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-        actions: [
-          TextButton(
-            child: Text('ライセンスを表示'),
-            onPressed: () {
-              showLicensePage(context: context);
-            },
-          ),
-          TextButton(
-            child: Text('閉じる'),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ],
-      );
-    },
-  );
 }

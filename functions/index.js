a39240f32401fdab34faa47e209b4a9f0c3cc6b6
beetana <beetana22beetana@gmail.com>
2021-03-22 20:59:11 +0900

    グループを削除するとトリガーされ、そのグループのサブコレクションを削除してくれる関数をcloud functionsに追加

--- functions/index.js
+++ functions/index.js
@@ -20,3 +20,21 @@ exports.deleteUser = functions
       console.error(err);
     }
   });
+
+exports.deleteGroup = functions
+  .region("asia-northeast1")
+  .firestore
+  .document("groups/{group}")
+  .onDelete(async (snap, context) => {
+    try {
+      await firebaseTools.firestore
+        .delete(snap.ref.path, {
+          project: process.env.GCLOUD_PROJECT,
+          recursive: true,
+          yes: true,
+          token: functions.config().fb.token
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  });

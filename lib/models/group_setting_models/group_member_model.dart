2433298cd4f4770a7800831f7355fa6f21baf680
beetana <beetana22beetana@gmail.com>
2021-03-22 20:57:33 +0900

    グループから退会した際、メンバーが0人になったらそのグループを削除する処理を追加

--- lib/models/group_setting_models/group_member_model.dart
+++ lib/models/group_setting_models/group_member_model.dart
@@ -1,15 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart' as Auth;
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 class GroupMemberModel extends ChangeNotifier {
   String groupId = '';
   String groupName = '';
+  String groupImageURL = '';
   String myId = '';
   List<String> usersId = [];
-  List<String> userNames = [];
-  List<String> userImageURLs = [];
+  List<String> usersName = [];
+  List<String> usersImageURL = [];
   bool isLoading = false;
+  final firestore = FirebaseFirestore.instance;
 
   void startLoading() {
     isLoading = true;
@@ -24,42 +27,85 @@ class GroupMemberModel extends ChangeNotifier {
   Future init({String groupId}) async {
     startLoading();
     this.groupId = groupId;
-    myId = Auth.FirebaseAuth.instance.currentUser.uid;
+    this.myId = Auth.FirebaseAuth.instance.currentUser.uid;
     try {
-      DocumentSnapshot groupDoc = await FirebaseFirestore.instance
-          .collection('groups')
-          .doc(groupId)
-          .get();
-      groupName = groupDoc['name'];
+      DocumentSnapshot groupDoc =
+          await firestore.collection('groups').doc(groupId).get();
+      this.groupName = groupDoc['name'];
+      this.groupImageURL = groupDoc['imageURL'];
+      await fetchGroupUsers();
+    } catch (e) {
+      print(e);
+    }
+    endLoading();
+  }
 
-      QuerySnapshot groupUsers = await FirebaseFirestore.instance
+  Future fetchGroupUsers() async {
+    try {
+      QuerySnapshot groupUsers = await firestore
           .collection('groups')
           .doc(groupId)
           .collection('groupUsers')
           .get();
-      usersId = (groupUsers.docs.map((doc) => doc.id).toList());
-      userNames =
+      this.usersId = (groupUsers.docs.map((doc) => doc.id).toList());
+      this.usersName =
           (groupUsers.docs.map((doc) => doc['name'].toString()).toList());
-      userImageURLs =
+      this.usersImageURL =
           (groupUsers.docs.map((doc) => doc['imageURL'].toString()).toList());
+      print(groupName);
+      print(groupUsers.size);
+      print(usersId);
+      print(usersName);
+      print(myId);
     } catch (e) {
       print(e);
-    } finally {
-      endLoading();
     }
   }
 
+  Future<int> checkMemberCount() async {
+    int memberCount;
+    try {
+      final groupUsersQuery = await firestore
+          .collection('groups')
+          .doc(groupId)
+          .collection('groupUsers')
+          .get();
+      memberCount = groupUsersQuery.size;
+    } catch (e) {
+      print(e);
+    }
+    return memberCount;
+  }
+
   Future deleteMember({String userId}) async {
-    final userDocRef =
-        FirebaseFirestore.instance.collection('users').doc(userId);
-    final groupDocRef =
-        FirebaseFirestore.instance.collection('groups').doc(groupId);
+    final userDocRef = firestore.collection('users').doc(userId);
+    final groupDocRef = firestore.collection('groups').doc(groupId);
     try {
       await userDocRef.collection('joiningGroup').doc(groupId).delete();
       await groupDocRef.collection('groupUsers').doc(userId).delete();
     } catch (e) {
       print(e);
       throw ('エラーが発生しました');
+    }
+  }
+
+  Future deleteGroup({String userId}) async {
+    try {
+      if (groupImageURL.isNotEmpty) {
+        await FirebaseStorage.instance
+            .ref()
+            .child("groupImage/$groupId")
+            .delete();
+      }
+      await firestore
+          .collection('users')
+          .doc(userId)
+          .collection('joiningGroup')
+          .doc(groupId)
+          .delete();
+      await firestore.collection('groups').doc(groupId).delete();
+    } catch (e) {
+      print(e);
     }
   }
 }

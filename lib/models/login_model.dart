49a1418af69da206ad085c4fc472ad1ee7446dcd
beetana <beetana22beetana@gmail.com>
2021-03-26 14:25:35 +0900

    welcome_modelをregister_modelとlogin_modelの二つに分けた

---
+++ lib/models/login_model.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' as Auth;
+
+class LoginModel extends ChangeNotifier {
+  String userId;
+  String email = '';
+  String password = '';
+  bool isLoading = false;
+  bool isAuthenticated;
+  final _auth = Auth.FirebaseAuth.instance;
+
+  void startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future login() async {
+    if (email.isEmpty) {
+      throw ('メールアドレスを入力してください');
+    }
+    if (password.isEmpty) {
+      throw ('パスワードを入力してください');
+    }
+
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      Auth.User user = _auth.currentUser;
+      userId = user.uid;
+    } catch (e) {
+      throw (_convertErrorMessage(e.code));
+    }
+  }
+}
+
+String _convertErrorMessage(e) {
+  switch (e) {
+    case 'invalid-email':
+      return 'メールアドレスを正しい形式で入力してください';
+    case 'wrong-password':
+      return 'パスワードが間違っています';
+    case 'user-not-found':
+      return 'ユーザーが見つかりません';
+    case 'user-disabled':
+      return 'ユーザーが無効です';
+    case 'too-many-requests':
+      return 'しばらく待ってからお試し下さい';
+    default:
+      return '不明なエラーです';
+  }
+}
---
+++ lib/models/register_model.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart' as Auth;
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RegisterModel extends ChangeNotifier {
+  String userId;
+  String name = '';
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
+  bool isAgree = false;
+  bool isLoading = false;
+  final _auth = Auth.FirebaseAuth.instance;
+
+  void startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void toggleCheckState() {
+    isAgree = !isAgree;
+    notifyListeners();
+  }
+
+  Future register() async {
+    if (name.isEmpty) {
+      throw ('アカウント名を入力してください');
+    }
+    if (email.isEmpty) {
+      throw ('メールアドレスを入力してください');
+    }
+    if (password.isEmpty) {
+      throw ('パスワードを入力してください');
+    }
+    if (confirmPassword.isEmpty) {
+      throw ('パスワード(確認用)を入力してください');
+    }
+    if (password != confirmPassword) {
+      throw ('パスワード(確認用)が一致しません');
+    }
+
+    try {
+      final Auth.User user = (await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      ))
+          .user;
+      await user.updateProfile(displayName: name);
+      userId = user.uid;
+      await FirebaseFirestore.instance.collection('users').doc(userId).set({
+        'name': name,
+        'imageURL': '',
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+    } catch (e) {
+      throw (_convertErrorMessage(e.code));
+    }
+  }
+}
+
+String _convertErrorMessage(e) {
+  switch (e) {
+    case 'invalid-email':
+      return 'メールアドレスを正しい形式で入力してください';
+    case 'email-already-in-use':
+      return 'そのメールアドレスはすでに使用されています';
+    case 'weak-password':
+      return 'パスワードは6文字以上で作成してください';
+    case 'too-many-requests':
+      return 'しばらく待ってからお試し下さい';
+    default:
+      return '不明なエラーです';
+  }
+}

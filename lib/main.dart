33a80b70f5a09c5add930cdb5d7befbd22194030
beetana <beetana22beetana@gmail.com>
2021-03-26 17:25:46 +0900

    画面の表示を縦方向のみに固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:beet/my_app.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   // main内で非同期処理を呼び出す場合runApp前に初期化が必要
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, //縦固定
+  ]);
   runApp(MyApp());
 }

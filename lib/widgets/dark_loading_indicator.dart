67dc4ac0f1d05dc7f84d7bedfae705fc49dbb342
beetana <beetana22beetana@gmail.com>
2021-03-28 22:13:56 +0900

    DarkLoadingIndicatorクラスを作成

---
+++ lib/widgets/dark_loading_indicator.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class DarkLoadingIndicator extends StatelessWidget {
+  final bool isLoading;
+
+  DarkLoadingIndicator({@required this.isLoading});
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? Container(
+            color: Colors.black.withOpacity(0.3),
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : SizedBox();
+  }
+}

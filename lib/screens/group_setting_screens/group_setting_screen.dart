f457812659930336fe1fce3c8fdf0f33d8df72c6
beetana <beetana22beetana@gmail.com>
2021-04-07 17:22:12 +0900

    設定画面にアプリの使い方の項目を作り、Q&A方式で使い方の説明を追加した

--- lib/screens/group_setting_screens/group_setting_screen.dart
+++ lib/screens/group_setting_screens/group_setting_screen.dart
@@ -2,6 +2,7 @@ import 'package:beet/constants.dart';
 import 'package:beet/models/group_setting_models/group_setting_model.dart';
 import 'package:beet/screens/group_setting_screens/group_member_screen.dart';
 import 'package:beet/screens/group_setting_screens/group_profile_screen.dart';
+import 'package:beet/screens/manual_screen.dart';
 import 'package:beet/screens/privacy_policy_screen.dart';
 import 'package:beet/screens/terms_screen.dart';
 import 'package:beet/utilities/show_app_info_dialog.dart';
@@ -55,9 +56,7 @@ class GroupSettingScreen extends StatelessWidget {
                   );
                 },
               ),
-              ThinDivider(
-                indent: 16.0,
-              ),
+              ThinDivider(indent: 16.0),
               ListTile(
                 title: const Text('メンバー'),
                 trailing: const Icon(Icons.keyboard_arrow_right),
@@ -89,6 +88,20 @@ class GroupSettingScreen extends StatelessWidget {
                 ),
               ),
               ListTile(
+                title: const Text('アプリの使い方'),
+                trailing: const Icon(Icons.keyboard_arrow_right),
+                onTap: () async {
+                  await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ManualScreen(),
+                      // UserProfileScreen(),
+                    ),
+                  );
+                },
+              ),
+              ThinDivider(indent: 16.0),
+              ListTile(
                 title: const Text('利用規約'),
                 trailing: const Icon(Icons.keyboard_arrow_right),
                 onTap: () {
@@ -101,9 +114,7 @@ class GroupSettingScreen extends StatelessWidget {
                   );
                 },
               ),
-              ThinDivider(
-                indent: 16.0,
-              ),
+              ThinDivider(indent: 16.0),
               ListTile(
                 title: const Text('プライバシーポリシー'),
                 trailing: const Icon(Icons.keyboard_arrow_right),
@@ -117,9 +128,7 @@ class GroupSettingScreen extends StatelessWidget {
                   );
                 },
               ),
-              ThinDivider(
-                indent: 16.0,
-              ),
+              ThinDivider(indent: 16.0),
               ListTile(
                 title: const Text('アプリの詳細'),
                 trailing: const Icon(Icons.keyboard_arrow_right),
---
+++ lib/screens/manual_screen.dart
@@ -0,0 +1,52 @@
+import 'package:beet/constants.dart';
+import 'package:beet/widgets/manual_list_tile.dart';
+import 'package:flutter/material.dart';
+
+class ManualScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kDullWhiteColor,
+      appBar: AppBar(
+        title: const Text('アプリの使い方'),
+      ),
+      body: SafeArea(
+        child: Scrollbar(
+          child: ListView(
+            physics: const ScrollPhysics(),
+            children: <Widget>[
+              ManualListTile(
+                question: '作成中のセットリストから曲を削除したい',
+                answer: 'セットリストの並べ替え画面で、曲やMCを長押しすることで削除できます。',
+              ),
+              ManualListTile(
+                question: 'グループを作りたい',
+                answer:
+                    '新規グループ作成の手順は以下の通りです。\n1.画面左上のメニューバーからメニューを開き、一番下の[グループを作成]をタップ。\n2.グループ名を入力し、決定ボタンをタップ。',
+              ),
+              ManualListTile(
+                question: 'グループにメンバーを招待したい',
+                answer:
+                    'メンバー招待の手順は以下の通りです。\n1.招待したいグループの画面を開く。\n2.右上の歯車アイコンから設定を開き、[メンバー]をタップ。\n3.一番下の[メンバーを招待]をタップ。\n4.招待メッセージが表示されるので、コピーして招待したい人に共有しましょう。',
+              ),
+              ManualListTile(
+                question: 'グループを削除したい',
+                answer:
+                    '直接グループを削除する機能はありませんが、グループを退会することができます。\nグループのメンバーが1人もいなくなった時点で、そのグループのデータは全て削除されます。\nグループを退会する手順は以下の通りです。\n1.退会したいグループの画面を開く。\n2.右上の歯車アイコンから設定を開き、[メンバー]をタップ。\n3.退会するアカウントの名前をタップ。\n4.[グループを退会]をタップ。',
+              ),
+              ManualListTile(
+                question: 'パスワードを忘れた',
+                answer: 'ログイン画面の[パスワードを忘れた場合]からパスワードの再設定を行ってください。',
+              ),
+              ManualListTile(
+                question: 'アカウントを削除してアプリを退会したい',
+                answer:
+                    'アカウント削除の手順は以下の通りです。\n1.マイページを開く。\n2.右上の歯車アイコンから設定を開き、[アカウント情報]をタップ。\n3.一番下の[アカウントを削除]をタップ。\n4.パスワードを入力し、[アカウントを削除]をタップ。',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
